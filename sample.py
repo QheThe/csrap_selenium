95e3b998f002176764eb6fc3c28ecb7db44427b4
2360527568 <2360527568@qq.com>
2019-06-26 11:24:52 +0800

    feat: cookie cache

--- sample.py
+++ sample.py
@@ -12,34 +12,34 @@ class page:
         self.cookies = []
         self.driver_options = driver_options
 
-    def open(self, url, cookie_chache_wait):
+    def open(self, url, cookie_cache_wait):
         driver = webdriver.Chrome(options=self.driver_options)
-        driver.get(url)
 
         if self.cookies:
+            driver.get(url)
             for cookie in self.cookies:
+                print('cookie 恢复')
+                print(cookie)
                 driver.add_cookie(cookie)
-            time.sleep(1)
-            driver.refresh()
-            print('cookie 恢复 浏览器刷新')
-                
-        raw_cookies = driver.get_cookies()
-        time.sleep(cookie_chache_wait)
-        for cookie in raw_cookies:
-            self.cookies.append({
-                'name': cookie['name'],
-                'value': cookie['value']
-            })
-        for cookie in self.cookies:
-            print('缓存 cookie')
-            print(cookie)
+        else:
+            driver.get(url)
+            raw_cookies = driver.get_cookies()
+            time.sleep(cookie_cache_wait)
+            for cookie in raw_cookies:
+                self.cookies.append({
+                    'name': cookie['name'],
+                    'value': cookie['value']
+                })
+            for cookie in self.cookies:
+                print('缓存 cookie')
+                print(cookie)
         return driver
 
 bilibili = page(driver_options=driverOptions)
-b1 = bilibili.open(url='https://www.bilibili.com', cookie_chache_wait=20)
+b1 = bilibili.open(url='https://www.bilibili.com', cookie_cache_wait=20)
 b1.quit()
 
 time.sleep(2)
 
-b2 = bilibili.open(url='https://www.bilibili.com', cookie_chache_wait=50)
+b2 = bilibili.open(url='https://www.bilibili.com', cookie_cache_wait=50)
 b2.quit()
