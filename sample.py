5e26b5d2d2afa0e1ea0ad89f76580c2c360072a1
2360527568 <2360527568@qq.com>
2019-06-26 11:01:16 +0800

    feat: cookie 模拟登录

--- sample.py
+++ sample.py
@@ -3,8 +3,8 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 driverOptions = Options()
-driverOptions.add_argument('--headless')
-driverOptions.add_argument('--disable-gpu')
+# driverOptions.add_argument('--headless')
+# driverOptions.add_argument('--disable-gpu')
 
 
 class page:
@@ -12,26 +12,34 @@ class page:
         self.cookies = []
         self.driver_options = driver_options
 
-    def get(self, url):
+    def open(self, url, cookie_chache_wait):
         driver = webdriver.Chrome(options=self.driver_options)
         driver.get(url)
-    
+
         if self.cookies:
             for cookie in self.cookies:
                 driver.add_cookie(cookie)
-        else:
-            raw_cookies = driver.get_cookies()
-            for cookie in raw_cookies:
-                self.cookies.append({
-                    'name': cookie['name'],
-                    'value': cookie['value']
-                })
+            time.sleep(1)
+            driver.refresh()
+            print('cookie 恢复 浏览器刷新')
+                
+        raw_cookies = driver.get_cookies()
+        time.sleep(cookie_chache_wait)
+        for cookie in raw_cookies:
+            self.cookies.append({
+                'name': cookie['name'],
+                'value': cookie['value']
+            })
+        for cookie in self.cookies:
+            print('缓存 cookie')
+            print(cookie)
         return driver
 
-
 bilibili = page(driver_options=driverOptions)
-b1 = bilibili.get('https://www.bilibili.com/')
+b1 = bilibili.open(url='https://www.bilibili.com', cookie_chache_wait=20)
 b1.quit()
-time.sleep(5)
-b2 = bilibili.get('https://www.bilibili.com/')
+
+time.sleep(2)
+
+b2 = bilibili.open(url='https://www.bilibili.com', cookie_chache_wait=50)
 b2.quit()
